4849b193a712154a78f5c16f97ac18a325c68854
Xinglin Song <songxingling1@outlook.com>
2025-07-12 14:23:19 +0800

    Pagination

--- components/Home.vue
+++ components/Home.vue
@@ -1,22 +1,35 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import { VCard,VCardTitle,VCardSubtitle,VCardText,VIcon } from 'vuetify/components';
+import { VPagination } from 'vuetify/components';
 import { useTheme } from 'vuetify';
-const articles = ref([])
+const showedArticles = ref([])
+let articles = []
 const theme = useTheme()
+const showPagination = ref(true)
+const pageCount = ref(1)
+const page = ref(2)
 onMounted(async ()=>{
     const res = await fetch('/document.json')
-    articles.value = await res.json()
+    articles = await res.json()
+    if(articles.length <= 10) showedArticles.value = articles
+    else {
+        showedArticles.value = articles.slice(0,10)
+        pageCount.value = Math.ceil(articles.length / 10)
+    }
 })
 function getDate(timestamp) {
     const timer = new Date(timestamp)
-    console.log(timestamp)
     return `${timer.getFullYear()}-${timer.getMonth() + 1}-${timer.getDate()}`
+}
+function updatePage(curPage) {
+    showedArticles.value = articles.slice((curPage - 1) * 10,curPage * 10)
+    console.log(curPage)
 }
 </script>
 <template>
 <div>
-    <VCard class="elevation-4 rounded-0 mb-3 pt-5 pb-5" v-for="(item,index) in articles" :key="index">
+    <VCard class="elevation-4 rounded-0 mb-3 pt-5 pb-5" v-for="(item,index) in showedArticles" :key="index">
         <VCardTitle class="text-center">
             <RouterLink :to="'/posts/' + item.name" class="text-h5 text-weight-medium opacity-100 pa-0 link">
                 {{item.name}}
@@ -27,6 +40,9 @@ function getDate(timestamp) {
             Posted on {{getDate(item.time)}}
         </VCardSubtitle>
         <VCardText class="pl-8 pr-8">{{ item.summary }}</VCardText>
+    </VCard>
+    <VCard v-if="showPagination" class="elevation-4 rounded-0">
+        <VPagination :length="pageCount" total-visible="3" v-model="page" @update:modelValue="updatePage(page)" ></VPagination>
     </VCard>
 </div>
 </template>
